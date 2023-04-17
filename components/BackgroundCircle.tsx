70081009c0093806db24636c3fac0f834676b5a8
Mona Lisa <87797742+Arisa-Kaewsuan@users.noreply.github.com>
2023-04-18 01:54:50 +0700

    add umg & discription

--- components/BackgroundCircle.tsx
+++ components/BackgroundCircle.tsx
@@ -13,13 +13,13 @@ function BackgroundCircle({}: Props) {
         borderRadius: ["20%", "20%", "50%", "80%", "20%"],
       }}
       transition={{ duration: 2.5 }}
-      className="relative flex justify-center items-center"
+      className="relative flex justify-center items-center "
     >
-      <div className="rounded-full border border-[#333333] absolute h-[200px] w-[200px] mt-52 animate-ping" />
-      <div className="rounded-full border border-[#333333] absolute h-[300px] w-[300px] mt-52 animate-ping" />
-      <div className="rounded-full border border-[#333333] absolute h-[500px] w-[500px] mt-52 animate-pulse" />
-      <div className="rounded-full border border-[#F7AB0A] absolute h-[650px] w-[650px] mt-52  opacity-20 animate-ping" />
-      <div className="rounded-full border border-[#333333] absolute h-[800px] w-[800px] mt-52  animate-ping" />
+      <div className="rounded-full border border-[#333333] absolute h-[200px] w-[200px] mt-80 animate-ping" />
+      <div className="rounded-full border border-[#333333] absolute h-[300px] w-[300px] mt-80 animate-ping" />
+      <div className="rounded-full border border-[#333333] absolute h-[500px] w-[500px] mt-80 animate-pulse" />
+      <div className="rounded-full border border-[#F7AB0A] absolute h-[650px] w-[650px] mt-80  opacity-20 animate-ping" />
+      <div className="rounded-full border border-[#333333] absolute h-[800px] w-[800px] mt-80  animate-ping" />
     </motion.div>
   );
 }
--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,6 +1,8 @@
 import React from "react";
+import Image from "next/legacy/image";
 import { Cursor, useTypewriter } from "react-simple-typewriter";
 import BackgroundCircle from "../components/BackgroundCircle";
+import Link from "next/link";
 
 type Props = {};
 
@@ -15,12 +17,33 @@ const Hero = (props: Props) => {
     delaySpeed: 2000,
   });
   return (
-    <div className="h-screen flex flex-col space-y-8 items-center justify-center text-center overflow-hidden">
+    <div className="h-screen flex flex-col space-y-8 items-center justify-center text-center overflow-hidden ">
       <BackgroundCircle />
-      <h1>
-        <span>{text}</span>
-        <Cursor cursorColor="#F7AB0A" />
-      </h1>
+      <Image
+        src="/pic.jpg"
+        width={200}
+        height={200}
+        className="relative rounded-full mx-auto object-cover"
+      />
+      <div className="z-20">
+        <h2 className="text-sm uppercase text-gray-500 pb-2 tracking-[15px]">
+          Arisa Kaewsuan
+        </h2>
+
+        <h1 className="text-5xl lg:text-6xl font-semibold px-10">
+          <span className="mr-3">{text}</span>
+          <Cursor cursorColor="#F7AB0A" />
+        </h1>
+
+        <div className="pt-5">
+          <Link href="#About">
+            <button className="heroButton">About</button>
+          </Link>
+          <Link href="#Projects">
+            <button className="heroButton">Projects</button>
+          </Link>
+        </div>
+      </div>
     </div>
   );
 };
