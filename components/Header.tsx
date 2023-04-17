8d93018ca4cf74105a3c5876868d85f216f4e156
Mona Lisa <87797742+Arisa-Kaewsuan@users.noreply.github.com>
2023-04-17 17:03:14 +0700

    add header navigation

--- components/Header.tsx
+++ components/Header.tsx
@@ -3,5 +3,44 @@ import React from "react";
 type Props = {};
 
 export default function Header({}: Props) {
-  return <div>Header</div>;
+  return (
+    <div className="bg-gray-700 h-screen ">
+      <div className="flex items-center justify-between">
+        <ul className="flex items-center justify-between">
+          <li className="mr-6">
+            <a className="text-gray-400 hover:text-blue-800" href="#">
+              github
+            </a>
+          </li>
+          <li className="mr-6">
+            <a className="text-gray-400 hover:text-blue-800" href="#">
+              linkedIn
+            </a>
+          </li>
+          <li className="mr-6">
+            <a className="text-gray-400 hover:text-blue-800" href="#">
+              leetcode
+            </a>
+          </li>
+          <li className="mr-6">
+            <a className="text-gray-400 hover:text-blue-800" href="#">
+              Behance
+            </a>
+          </li>
+          <li className="mr-6">
+            <a className="text-gray-400 hover:text-blue-800" href="#">
+              Medium
+            </a>
+          </li>
+        </ul>
+        <ul>
+          <li className="mr-6">
+            <a className="text-gray-400 hover:text-blue-800" href="#">
+              GET IN TOUCH
+            </a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
 }
