1f2a9f24d4e2b68543d8da575f91f51fc7d84731
Mona Lisa <87797742+Arisa-Kaewsuan@users.noreply.github.com>
2023-04-18 02:45:17 +0700

    add About page

---
+++ components/About.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { motion } from "framer-motion";
+
+type Props = {};
+
+const About = (props: Props) => {
+  return (
+    <div className="h-screen flex relative flex-col text-center md:text-left md:flex-row max-w-7xl px-10 justify-evenly mx-auto items-center">
+      <h3 className="absolute top-24  uppercase tracking-[20px] text-gray-500 text-2xl">
+        About
+      </h3>
+
+      <motion.img
+        initial={{
+          x: -200,
+          opacity: 0,
+        }}
+        transition={{ duration: 1.2 }}
+        whileInView={{ opacity: 1, x: 0 }}
+        viewport={{ once: true }}
+        src="/pic.jpg"
+        className="w-56 h-56 rounded-full flex-shrink-0 object-cover mt-20 -mb-20 md:mb-0  md:rounded-lg md:w-64 md:h-95 xl:w-[300px] xl:h-[400px]"
+      />
+    </div>
+  );
+};
+
+export default About;
--- pages/index.tsx
+++ pages/index.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import Head from "next/head";
 import Header from "@/components/Header";
 import Hero from "@/components/Hero";
+import About from "@/components/About";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -17,11 +18,14 @@ export default function Home() {
       <Header />
 
       {/* Hero */}
-      <section id="hero" className="snap-center">
+      <section id="hero" className="snap-start">
         <Hero />
       </section>
 
       {/* About */}
+      <section id="about" className="snap-center">
+        <About />
+      </section>
 
       {/* Experience */}
 
