0c5f3250550af2a78e141f9cdbcfaf936cc5c2c1
Mona Lisa <87797742+Arisa-Kaewsuan@users.noreply.github.com>
2023-04-24 23:41:43 +0700

    edit

--- pages/index.tsx
+++ pages/index.tsx
@@ -11,7 +11,7 @@ export default function Home() {
   return (
     <div className="bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory overflow-scroll ">
       <Head>
-        <title> mapleJourney </title>
+        <title> a.maple_journey </title>
       </Head>
 
       {/* Header */}
